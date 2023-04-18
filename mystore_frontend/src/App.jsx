8dfb3b9632dcb80863718647313e07bbcf7164f7
Jhon Alexander Valencia <alexandervalencia0121@gmail.com>
2023-04-17 19:54:45 -0500

    Upload the project

---
+++ mystore_frontend/src/App.jsx
@@ -0,0 +1,75 @@
+import { useState } from 'react'
+import { Form, Button } from "react-bootstrap";
+import './App.css'
+import superstoreimg from './superstore.png';
+
+export const App = () => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [categories, setCategories] = useState([]);
+
+  const handleSubmit = (event) => {
+    
+    event.preventDefault();
+    // Realizar petición POST con los campos "name" y "description"
+    fetch("http://localhost:4000/categories/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, description }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+    event.target.reset();
+}
+
+const handleGetCategories = () => {
+  // Realizar petición GET para obtener las categorías
+  fetch("http://localhost:4000/categories/")
+    .then((response) => response.json())
+    .then((data) => setCategories(data));
+};
+
+  return (
+    <div className="App">
+      <img src={superstoreimg} alt="Super Store" className="imagen" />
+
+         <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="formName">
+        <Form.Label>Name</Form.Label>
+        <Form.Control 
+           type="text" 
+           value={name} 
+           onChange={(e) => setName(e.target.value)} 
+        />
+      </Form.Group>
+
+      <Form.Group controlId="formDescription">
+        <Form.Label>Description</Form.Label>
+        <Form.Control 
+            type="text" 
+            value={description} 
+            onChange={(e) => setDescription(e.target.value)} 
+        />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Send
+      </Button>
+      <Button variant="second" onClick={handleGetCategories}>
+        Get Categories 
+      </Button>
+    </Form>
+    {categories.length > 0 && (
+        <ul>
+          {categories.map((category) => (
+            <li key={category.id}>
+              {category.name} - {category.description}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  )
+}
+
+
